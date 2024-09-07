beb0eff0c18d1e55279ebbf01753bc2c97f848b9
lakshaybabbar <lakshaybabbar0118@gmail.com>
2024-09-07 14:33:56 +0530

    Added socket code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,13 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  try {
+    return res.status(200).json({ message: "Hello World" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
 export default router;
